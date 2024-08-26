5d61ab892828cdf03dee8f2f3ce3e674b3c09269
Evan Gagnon <egwiz01@gmail.com>
2024-08-26 13:48:30 -0400

    Added images

--- Code/windowCreator.py
+++ Code/windowCreator.py
@@ -1,6 +1,8 @@
 from PIL import Image
+import os
+directory = "ISpy-Collage-main\ISpy-Collage\Images"
+##im = Image.open("ISpy-Collage-main\ISpy-Collage\Images\Screenshot 2024-07-16 160455.png")
+##im.show()
 
-im = Image.open("Screenshot 2024-07-16 160455.png")
-im.show()
-
+##zip(*())
 
